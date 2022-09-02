91a44053ccf8ed343424253f5c12d2464e32a2bc
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-09-02 21:59:17 +0900

    7장 배열 실습

---
+++ C/C 복습/배열 - x 행렬에서 y 행렬 찾기.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+	int x[3][3], y[2][2], i, j, r=-1, c=-1;
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++)
+			scanf("%d", &x[i][j]);
+	}
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++)
+			scanf("%d", &y[i][j]);
+	}
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++) {
+			if (x[i][j] == y[0][0] && x[i + 1][j] == y[1][0] && x[i][j + 1] == y[0][1] && x[i + 1][j + 1] == y[1][1]) {
+				r = i;
+				c = j;
+			}
+		}
+	}
+	if (r>=0) printf("%d %d", r, c);
+	else printf("none");
+	return 0;
+}
