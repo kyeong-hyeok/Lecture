ec91069e442fb595fe1af44a2124c448a30339cb
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-09-26 11:14:10 +0900

    객체지향 예제

---
+++ JAVA/객체지향/예제 6-1,2 Tv 클래스 활용하기.java
@@ -0,0 +1,36 @@
+package ch6exTv;
+
+class Tv {
+	String color;
+	boolean power;
+	int channel;
+	void power() {power = !power;}
+	void channelUp(){ channel++;}
+	void channelDown() { channel--;}
+}
+
+public class TvTest {
+
+	public static void main(String[] args) {
+		//ex6-1
+		Tv t = new Tv();
+		System.out.println("After instantiation, color : " +t.color
+				+ ", power : " +t.power
+				+ ", channel : " +t.channel);
+		t.channel = 7;
+		t.channelDown();
+		System.out.println("t's channel : " + t.channel);
+		//ex6-2
+		Tv t1 = new Tv();
+		Tv t2 = new Tv();
+		t1.channel = 5;
+		t2.channel = 6;
+		System.out.println("t1's channel : " + t1.channel);
+		System.out.println("t2's channel : " + t2.channel);
+		t2=t1;
+		//t1.channel = 7;
+		System.out.println("t1's channel : " + t1.channel);
+		System.out.println("t2's channel : " + t2.channel);
+	}
+
+}
