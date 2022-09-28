cd2e45867029c38cd344364cc48421960df59bac
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-09-28 11:10:51 +0900

    객체지향 예제

---
+++ JAVA/객체지향/예제 6-5 CardTest 클래스.java
@@ -0,0 +1,41 @@
+package ch6ex5;
+
+public class CardTest {
+
+	public static void main(String[] args) {
+		System.out.println("Card.width = "+Card.width
+				+", Card.height = "+Card.height);
+		Card c1 = new Card();
+		c1.kind = "Heart";
+		c1.number = 7;
+		Card c2 = new Card();
+		c2.kind = "Spade";
+		c2.number = 4;
+		System.out.println("c1.kind = "+c1.kind 
+				+", c1.number = " +c1.number
+				+", c1.width = "+c1.width
+				+", c1.height = "+c1.height);
+		System.out.println("c1.kind = "+c2.kind 
+				+", c2.number = " +c2.number
+				+", c2.width = "+c2.width
+				+", c2.height = "+c2.height);
+		c1.width = 50;
+		c1.height = 80;
+		System.out.println("c1.kind = "+c1.kind 
+				+", c1.number = " +c1.number
+				+", c1.width = "+c1.width
+				+", c1.height = "+c1.height);
+		System.out.println("c1.kind = "+c2.kind 
+				+", c2.number = " +c2.number
+				+", c2.width = "+c2.width
+				+", c2.height = "+c2.height);				
+	}
+
+}
+
+class Card {
+	String kind;
+	int number;
+	static int width = 100;
+	static int height = 250;
+}
