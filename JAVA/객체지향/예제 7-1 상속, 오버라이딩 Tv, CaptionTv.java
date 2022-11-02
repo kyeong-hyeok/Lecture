6aea5f556768cf7952e98c6668d1b477d4559810
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-11-02 11:20:02 +0900

    객체지향 예제

---
+++ JAVA/객체지향/예제 7-1 상속, 오버라이딩 Tv, CaptionTv.java
@@ -0,0 +1,62 @@
+package ch7ex1;
+
+class Tv {
+	boolean power;
+	int channel;
+	void changePower() {power=!power;}
+	void channelUp() {
+		if(power)
+			++channel;
+	}
+	void channelDown() {
+		if(power)
+			--channel;
+	}
+	public String toString() {
+		return "power = "+power+", channel = "+channel;
+	}
+}
+
+class CaptionTv extends Tv {
+	boolean caption;
+	void displayCaption(String text) {
+		if(power && caption)
+			System.out.println(text);
+	}
+	public String toString() {
+		return "power = "+power+", channel = "+channel
+				+", caption = "+caption;
+	}
+}
+
+public class CaptionTvTest {
+	public static void main(String[] args) {
+		Tv tv = new Tv();
+		System.out.println(tv);
+		System.out.println(tv.toString());
+		
+		CaptionTv ctv = new CaptionTv();
+		System.out.println(ctv);
+		/*System.out.println("ctv.power = "+ctv.power
+				+", ctv.channel = "+ctv.channel
+				+", ctv.caption = "+ctv.caption);*/
+		ctv.changePower();
+		System.out.println(ctv);
+		/*System.out.println("ctv.power = "+ctv.power
+				+", ctv.channel = "+ctv.channel
+				+", ctv.caption = "+ctv.caption);*/
+		ctv.channel = 10;
+		ctv.channelUp();
+		System.out.println(ctv);
+		/*System.out.println("ctv.power = "+ctv.power
+				+", ctv.channel = "+ctv.channel
+				+", ctv.caption = "+ctv.caption);*/
+		ctv.displayCaption("Hello, world!");
+		ctv.caption=true;
+		System.out.println(ctv);
+		/*System.out.println("ctv.power = "+ctv.power
+				+", ctv.channel = "+ctv.channel
+				+", ctv.caption = "+ctv.caption);*/
+		ctv.displayCaption("Hello, world!");
+	}
+}
